02df6de128fb43018db8ed48c02973a7d1efa569
LAB01 <LAB01@PC-13>
2020-01-24 20:11:50 -0600

    Prueba

---
+++ RecursividadEjemploUno/src/main/java/MainClass.java
@@ -0,0 +1,10 @@
+public class MainClass {
+  public static void main (String [] args) {
+       DoJob ();
+   }
+   private static void DoJob (){
+       RecursividadEjemploUno ejemploRecursivo =
+               new RecursividadEjemploUno ();
+       ejemploRecursivo.EjecutarEjemplouno ();
+   }    
+}
---
+++ RecursividadEjemploUno/src/main/java/RecursividadEjemploUno.java
@@ -0,0 +1,15 @@
+public class RecursividadEjemploUno {
+    private void CapturarString(){
+        
+    }
+    private void Imprimir_Derecha_Izquierda() {
+        
+    }
+    private void Imprimir_Recursivo () {
+        
+    }
+    public void EjecutarEjemplouno () {
+        CapturarString ();
+        Imprimir_Derecha_Izquierda ();
+    }    
+}
